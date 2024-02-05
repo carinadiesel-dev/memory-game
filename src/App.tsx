8766e477b6ca9242e9966240dbb523b778851d32
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2024-02-05 13:35:30 +0200

    Basic User Name Modal Complete

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,16 @@ import { Box } from "@mui/material";
 import { useState } from "react";
 import "./App.css";
 import backgroundImage from "./assets/Background.png";
+import UserNamesModal from "./components/UserNamesModal";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <Box sx={{ backgroundImage: `url(${backgroundImage})` }}>Hello World</Box>
+      <Box sx={{ backgroundImage: `url(${backgroundImage})` }}>
+        <UserNamesModal />
+      </Box>
     </>
   );
 }

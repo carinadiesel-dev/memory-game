1bfb551d3b1ee01c7d40d7b9c6ef48a0028fcd43
Carina <106066035+carinadiesel-dev@users.noreply.github.com>
2024-02-06 13:44:16 +0200

    Added card constants

---
+++ src/common/constants.tsx
@@ -0,0 +1,273 @@
+const HEARTS = [
+  {
+    value: "King",
+    suit: "Hearts",
+    image: "../assets/cards/heart-k.svg",
+  },
+  {
+    value: "Queen",
+    suit: "Hearts",
+    image: "../assets/cards/heart-q.svg",
+  },
+  {
+    value: "Jack",
+    suit: "Hearts",
+    image: "../assets/cards/heart-j.svg",
+  },
+  {
+    value: "10",
+    suit: "Hearts",
+    image: "../assets/cards/heart-10.svg",
+  },
+  {
+    value: "9",
+    suit: "Hearts",
+    image: "../assets/cards/heart-9.svg",
+  },
+  {
+    value: "8",
+    suit: "Hearts",
+    image: "../assets/cards/heart-8.svg",
+  },
+  {
+    value: "7",
+    suit: "Hearts",
+    image: "../assets/cards/heart-7.svg",
+  },
+  {
+    value: "6",
+    suit: "Hearts",
+    image: "../assets/cards/heart-6.svg",
+  },
+  {
+    value: "5",
+    suit: "Hearts",
+    image: "../assets/cards/heart-5.svg",
+  },
+  {
+    value: "4",
+    suit: "Hearts",
+    image: "../assets/cards/heart-4.svg",
+  },
+  {
+    value: "3",
+    suit: "Hearts",
+    image: "../assets/cards/heart-3.svg",
+  },
+  {
+    value: "2",
+    suit: "Hearts",
+    image: "../assets/cards/heart-2.svg",
+  },
+  {
+    value: "Ace",
+    suit: "Hearts",
+    image: "../assets/cards/heart-a.svg",
+  },
+];
+
+const DIAMONDS = [
+  {
+    value: "King",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-k.svg",
+  },
+  {
+    value: "Queen",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-q.svg",
+  },
+  {
+    value: "Jack",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-j.svg",
+  },
+  {
+    value: "10",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-10.svg",
+  },
+  {
+    value: "9",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-9.svg",
+  },
+  {
+    value: "8",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-8.svg",
+  },
+  {
+    value: "7",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-7.svg",
+  },
+  {
+    value: "6",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-6.svg",
+  },
+  {
+    value: "5",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-5.svg",
+  },
+  {
+    value: "4",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-4.svg",
+  },
+  {
+    value: "3",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-3.svg",
+  },
+  {
+    value: "2",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-2.svg",
+  },
+  {
+    value: "Ace",
+    suit: "Diamonds",
+    image: "../assets/cards/diamond-a.svg",
+  },
+];
+
+const SPADES = [
+  {
+    value: "King",
+    suit: "Spades",
+    image: "../assets/cards/spade-k.svg",
+  },
+  {
+    value: "Queen",
+    suit: "Spades",
+    image: "../assets/cards/spade-q.svg",
+  },
+  {
+    value: "Jack",
+    suit: "Spades",
+    image: "../assets/cards/spade-j.svg",
+  },
+  {
+    value: "10",
+    suit: "Spades",
+    image: "../assets/cards/spade-10.svg",
+  },
+  {
+    value: "9",
+    suit: "Spades",
+    image: "../assets/cards/spade-9.svg",
+  },
+  {
+    value: "8",
+    suit: "Spades",
+    image: "../assets/cards/spade-8.svg",
+  },
+  {
+    value: "7",
+    suit: "Spades",
+    image: "../assets/cards/spade-7.svg",
+  },
+  {
+    value: "6",
+    suit: "Spades",
+    image: "../assets/cards/spade-6.svg",
+  },
+  {
+    value: "5",
+    suit: "Spades",
+    image: "../assets/cards/spade-5.svg",
+  },
+  {
+    value: "4",
+    suit: "Spades",
+    image: "../assets/cards/spade-4.svg",
+  },
+  {
+    value: "3",
+    suit: "Spades",
+    image: "../assets/cards/spade-3.svg",
+  },
+  {
+    value: "2",
+    suit: "Spades",
+    image: "../assets/cards/spade-2.svg",
+  },
+  {
+    value: "Ace",
+    suit: "Spades",
+    image: "../assets/cards/spade-a.svg",
+  },
+];
+
+const CLUBS = [
+  {
+    value: "King",
+    suit: "Clubs",
+    image: "../assets/cards/club-k.svg",
+  },
+  {
+    value: "Queen",
+    suit: "Clubs",
+    image: "../assets/cards/club-q.svg",
+  },
+  {
+    value: "Jack",
+    suit: "Clubs",
+    image: "../assets/cards/club-j.svg",
+  },
+  {
+    value: "10",
+    suit: "Clubs",
+    image: "../assets/cards/club-10.svg",
+  },
+  {
+    value: "9",
+    suit: "Clubs",
+    image: "../assets/cards/club-9.svg",
+  },
+  {
+    value: "8",
+    suit: "Clubs",
+    image: "../assets/cards/club-8.svg",
+  },
+  {
+    value: "7",
+    suit: "Clubs",
+    image: "../assets/cards/club-7.svg",
+  },
+  {
+    value: "6",
+    suit: "Clubs",
+    image: "../assets/cards/club-6.svg",
+  },
+  {
+    value: "5",
+    suit: "Clubs",
+    image: "../assets/cards/club-5.svg",
+  },
+  {
+    value: "4",
+    suit: "Clubs",
+    image: "../assets/cards/club-4.svg",
+  },
+  {
+    value: "3",
+    suit: "Clubs",
+    image: "../assets/cards/club-3.svg",
+  },
+  {
+    value: "2",
+    suit: "Clubs",
+    image: "../assets/cards/club-2.svg",
+  },
+  {
+    value: "Ace",
+    suit: "Clubs",
+    image: "../assets/cards/club-a.svg",
+  },
+];
+
+const CARDS = [...HEARTS, ...DIAMONDS, ...SPADES, ...CLUBS];
